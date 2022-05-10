d0f258d10a9e46b851dde208f594919f3062956f
Duc Tai <vo1122000@gmail.com>
2022-05-10 21:11:40 +0700

    first commit

---
+++ admin2/src/pages/login/Login.jsx
@@ -0,0 +1,31 @@
+import "./login.scss";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+  return (
+    <div className="login">
+      <p className="title">Jovinn.</p>
+      <div className="form">
+        <TextField className="input" label="username" variant="outlined" />
+        <TextField className="input" label="password" variant="outlined" />
+
+        <Button variant="outlined" className="btn" onClick={handleLogin}>
+          Login
+        </Button>
+        {/* <Alert severity="error" className="error" disabled>
+          Username or Password is wrong!
+        </Alert> */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
